7186a541bb730940bc71636b9e511a5401a0413c
PercTruman <drshawnhart@gmail.com>
2022-05-04 21:13:40 -0500

    got the form submit button working, and is persisting in the database

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
-function sayHi(){
-    console.log("hi")
-}
+document.addEventListener('DOMContentLoaded', ()=>{
+    fetch('http://localhost:3000/users')
+        .then(res=>res.json())
+        .then(data=>console.log(data))
+})
 
-sayHi()
+const newMemberForm=document.getElementById('new-member-form')
+
+
+newMemberForm.addEventListener('submit',
+    (e)=>{
+        e.preventDefault() 
+        let firstNameField=e.target[0].value
+        let lastNameField=e.target[1].value
+        let emailField=e.target[2].value
+        let phoneField=e.target[3].value
+        let subcategoryField=e.target[4].value
+        
+        let memberObject={
+            firstname: firstNameField,
+            lastname: lastNameField,
+            email: emailField,
+            phonenumber: phoneField,
+            instrument: subcategoryField
+        }
+        postNewMember(memberObject)
+    }
+)
+
+function postNewMember(memberObject){
+    fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+    },
+        body:JSON.stringify(memberObject)
+})
+    .then(res=>console.log(res.json()))
+    // .then(member=)
+}
